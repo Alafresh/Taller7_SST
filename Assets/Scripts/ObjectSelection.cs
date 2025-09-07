8454f62234701f4c6de770f05316923cd0d92886
andruAJ <giraldoandresjuan0405@gmail.com>
2025-09-06 19:20:31 -0500

    Building blocks básicos agregados

---
+++ Assets/Scripts/ObjectSelection.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ObjectSelection : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
