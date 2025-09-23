7854ea99f471f51a1d605e0786e991510c653b22
andruAJ <giraldoandresjuan0405@gmail.com>
2025-09-23 13:18:26 -0500

    Quitado script innecesario

--- Assets/Scripts/ObjectSelection.cs
+++ Assets/Scripts/ObjectSelection.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ObjectSelection : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject pinkParticles;    [SerializeField] private GameObject blueParticles;   [SerializeField] private GameObject yellowParticles; [SerializeField] private GameObject greenParticles; [SerializeField] private GameObject orangeParticles;  // partículas de colores
     void Start()
     {
         
